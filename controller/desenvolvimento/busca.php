e07766f12da1b19fe2f174fe1d95af2212567fdb
DESKTOP-EJDG6RT\emerson <emersom_carvalho@hotmail.com>
2018-06-17 20:34:04 -0300

    att1

--- controller/desenvolvimento/busca.php
+++ controller/desenvolvimento/busca.php
@@ -12,7 +12,7 @@
 <form name="searchform" method="post" action="resultado.php">
 	
 	<center>Pesquisar usuário: <input type="text" name="buscar" /></center>
-	<br><br>
+	
 	<center><input type="submit" value="Buscar"></center>
 </form>
 </body>
